3c630b525dbb8037cc361fcdb1f0bf9972adf3f3
YevhenBashkatov <yevhen.bashkatov@nure.ua>
2020-09-15 23:03:42 +0300

    Added logic of lesson 12

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,2 +1,6 @@
 class RailwayStation < ApplicationRecord
+  has_many :railway_station_routes
+  has_many :routes, :through => :railway_station_routes
+  has_many :trains
+  has_many :tickets
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,6 @@
 class Route < ActiveRecord::Base
   validates :name, presence: true
+  has_many :railway_station_routes
+  has_many :railway_stations, :through => :railway_station_routes
+  has_many :trains
 end
